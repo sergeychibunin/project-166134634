e0b6f0fee9c50a8b96e58a6e7523c4a08444b08f
Sergey Chibunin <chibunin1988@gmail.com>
2020-10-22 19:05:35 +0300

    Add tests: logining, post creation

--- socnet/core/tests.py
+++ socnet/core/tests.py
@@ -3,6 +3,7 @@ from rest_framework.test import APITestCase
 from django.urls import reverse
 from django.contrib import auth
 from django.contrib.auth.models import User
+from core.models import Post
 
 
 class SigningUpTestCase(APITestCase):
@@ -24,3 +25,41 @@ class SigningUpTestCase(APITestCase):
             {'username': 'user1', 'password': 'password123'}
         )
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+
+class LoginingTestCase(APITestCase):
+
+    def test_user_can_log_in(self):
+        credentials = {'username': 'user1', 'password': 'password123'}
+        self.client.post(reverse('signup'), credentials)
+        response = self.client.post(
+            reverse('token_obtain_pair'),
+            credentials
+        )
+        user = auth.get_user(self.client)
+        self.assertEqual(user.is_authenticated, False)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual('access' in response.data, True)
+        self.assertEqual(len(response.data['access']) > 0, True)
+
+
+class UserActivityTestCase(APITestCase):
+
+    def setUp(self):
+        credentials = {'username': 'user1', 'password': 'password123'}
+        self.client.post(reverse('signup'), credentials)
+        response = self.client.post(
+            reverse('token_obtain_pair'),
+            credentials
+        )
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {response.data["access"]}')
+
+    def test_user_can_create_post(self):
+        self.assertEqual(Post.objects.count() == 0, True)
+        
+        response = self.client.post(
+            reverse('post'),
+            {'body': 'Hello'}
+        )
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(Post.objects.count() == 1, True)
