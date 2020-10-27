879cd017c19b64feffd22af37beae35a7a181bce
Sergey Chibunin <chibunin1988@gmail.com>
2020-10-27 16:06:32 +0300

    Add the bot. WIP

--- bot.py
+++ bot.py
@@ -1,7 +1,38 @@
 import time
-from typing import Dict
+import random
+from typing import Dict, List
 import configparser
-from rest_framework.test import RequestsClient
+import requests
+import string
+
+
+def rnd_word() -> str:
+    """
+    Make an undefined word
+    """
+    return ''.join(random.choice(string.ascii_letters) for _ in range(4))
+
+
+def rnd_text() -> str:
+    """
+    Make a random text
+    """
+    return ' '.join(rnd_word() for _ in range(8))
+
+
+class Store:
+    srv_uri: str
+    users: List[Dict]
+
+
+store = Store()
+
+
+def cfg_srv_init(cfg: configparser.SectionProxy) -> None:
+    """
+    Save the server URI
+    """
+    store.srv_uri = f'http://{cfg["server_host"]}{cfg["server_port"]}'
 
 
 def rand_user(number: int) -> Dict[str, str]:
@@ -17,8 +48,16 @@ def signup(user: Dict[str, str]) -> None:
     """
     Sign up a new user
     """
-    client = RequestsClient()
-    client.post(f'{cfg_srv()}/api/user/signup', json=user)
+    requests.post(f'{store.srv_uri}/api/user/signup', data=user)
+
+
+def login(user_number: int) -> None:
+    """
+    Log in an user
+    """
+    user = store.users[user_number]
+    response = requests.post(f'{store.srv_uri}/api/token/', data=user)
+    store.users[user_number]['token'] = response.json()['access']
 
 
 def add_users(amount: int) -> None:
@@ -27,7 +66,17 @@ def add_users(amount: int) -> None:
     """
     for number in amount:
         user = rand_user(number)
+        store.users.append(user)
         signup(user)
+        print(f'Welcome {user["username"]}')
+
+
+def write_posts(post_limit: int) -> None:
+    """
+    Generate random posts
+    """
+    for user_number in range(len(store.users)):
+        login(user_number)
 
 
 def main() -> None:
@@ -35,8 +84,8 @@ def main() -> None:
     config.read('bot.ini')
     cfg_srv_init(config['network'])
     add_users(int(config['behavior']['number_of_users']))
-    write_posts(config['behavior']['max_posts_per_user'])
-    add_likes(config['behavior']['max_likes_per_user'])
+    # write_posts(config['behavior']['max_posts_per_user'])
+    # add_likes(config['behavior']['max_likes_per_user'])
 
 
 if __name__ == "__main__":
